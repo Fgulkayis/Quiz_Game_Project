8d2514cb949d71efff75296c3e8f74e2270b5406
Fatma Gül Kayış <165270103+Fgulkayis@users.noreply.github.com>
2025-08-01 11:39:59 +0300

    fix: Implemented dynamic room joining logic on the frontend

--- game/views.py
+++ game/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Room, Player
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
 
+@login_required
 def index(request):
-    return render(request, 'game/index.html')
+    
+    rooms = Room.objects.all()
+    players = Player.objects.all()
+    return render(request, 'game/index.html', {'rooms': rooms, 'players': players})
+
+@login_required
+def room(request, room_name):
+    
+    room, created = Room.objects.get_or_create(name=room_name)
+    
+    
+    player, created = Player.objects.get_or_create(user=request.user)
+    player.room = room
+    player.save()
+    
+    return render(request, 'game/room.html', {'room_name': room_name})
